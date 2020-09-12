baf01ba0d48b88669276345c9c1f132636bead78
Jacquelyn Chow <icecreamjackie@gmail.com>
2020-09-12 18:58:48 -0400

    single entry page done + record button is linked, for now i hardcoded the title, date, entry contents

--- app/routes.py
+++ app/routes.py
@@ -3,10 +3,16 @@ from app import app
 from datetime import date
 
 @app.route('/')
-@app.route('/index', methods=["GET"])
+@app.route('/index', methods=["GET", "POST"])
 def index():
     today = date.today()
-    return render_template('index.html', title='Home', date=today)
+    # if user submitted an entry, get it from the form and re-route to the entry page
+    if request.method == 'POST':
+        entry_details = request.form.get('entry')
+        entry_title = request.form.get('entry-title')
+        print(entry_title)
+        return redirect(url_for('saveData', entry_details=entry_details, entry_title=entry_title))
+    return render_template('index.html', date=today)
 
 @app.route('/login', methods=["GET"])
 def login():
@@ -26,9 +32,17 @@ def record():
 
 @app.route("/save")
 def saveData():
+    entry_title = request.args.get('entry_title')
     entry_details = request.args.get('entry_details')
-    return render_template('save.html', entry_details=entry_details)
+    return render_template('save.html', entry_details=entry_details, entry_title=entry_title)
 
 @app.route('/entries')
 def entries():
     return render_template('entries.html')
+
+@app.route('/entry')
+def entry():
+    title = "This is my title"
+    date = "09/12/2020"
+    entry_details = "Here is my example entry."
+    return render_template('singleEntry.html', title=title, date=date, entry_details=entry_details)
