00b6e3178c03479aa83e004e133352242b2f5149
Jacquelyn Chow <icecreamjackie@gmail.com>
2020-09-12 13:36:28 -0400

    login adn sign up page layout done

--- app/routes.py
+++ app/routes.py
@@ -6,3 +6,11 @@ from app import app
 def index():
     user = {'username': 'Alice'}
     return render_template('index.html', title='Home', user=user)
+
+@app.route('/login', methods=["GET"])
+def login():
+    return render_template('login.html')
+
+@app.route('/signup', methods=["GET"])
+def signup():
+    return render_template('signup.html')
